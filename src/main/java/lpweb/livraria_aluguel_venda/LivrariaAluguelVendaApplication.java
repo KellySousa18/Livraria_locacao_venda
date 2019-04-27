5cc83379712ee09c1a5810fbd4c8d510a7fb9c79
KELLY SOUSA <KELLY.SOUSA@ACAD.IFMA.EDU.BR>
2019-04-27 00:22:08 -0300

    Livraria Teste

---
+++ src/main/java/lpweb/livraria_aluguel_venda/LivrariaAluguelVendaApplication.java
@@ -0,0 +1,13 @@
+package lpweb.livraria_aluguel_venda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaAluguelVendaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LivrariaAluguelVendaApplication.class, args);
+    }
+
+}
